3cb6da8f10d9e6f25354b4f40b3624146c55b1a3
unknown <sushmitabhor3@gmail.com>
2023-08-28 22:43:21 +0530

    refactor done

--- lib/description.dart
+++ lib/description.dart
@@ -11,53 +11,49 @@ class Description extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kbackgroundColor,
-      body: Container(
-        child:
-
-        ListView(
+      body: ListView(
+        children: [
+          Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Positioned(child: Container(
+                  height: 250,
+                  width: MediaQuery.of(context).size.width,
+                  child:CachedNetworkImage(imageUrl: 'http://image.tmdb.org/t/p/w500' + posterurl ,fit: BoxFit.cover,
+                    placeholder:(context,val)=>const Center(child: CircularProgressIndicator(),),errorWidget: (context,url,error)=>const Icon(Icons.error),)
+           )),
+                Positioned(
+                    bottom: 10,
+                    child: modified_text(text: 'Average Rating\t - '+vote,size: 18,))
+              ],
+            ),
+          ),
+          const SizedBox(height: 15,),
+          Container(padding: const EdgeInsets.all(10),
+          child: modified_text(text: name!=null?name:"Not Loaded",size: 14,),
+          ),
+          Container(padding: const EdgeInsets.only(left: 10),child: modified_text(text: 'Releasing on\t-'+launch_on,size: 14,),)
+       , Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: 250,
-              child: Stack(
-                children: [
-                  Positioned(child: Container(
-                    height: 250,
-                    width: MediaQuery.of(context).size.width,
-                    child:CachedNetworkImage(imageUrl: 'http://image.tmdb.org/t/p/w500' + posterurl ,fit: BoxFit.cover,
-                      placeholder:(context,val)=>Center(child: CircularProgressIndicator(),),errorWidget: (context,url,error)=>const Icon(Icons.error),)
-             )),
-                  Positioned(
-                      bottom: 10,
-                      child: modified_text(text: 'Average Rating\t - '+vote,size: 18,))
-                ],
-              ),
-            ),
-            const SizedBox(height: 15,),
-            Container(padding: EdgeInsets.all(10),
-            child: modified_text(text: name!=null?name:"Not Loaded",size: 14,),
-            ),
-            Container(padding: EdgeInsets.only(left: 10),child: modified_text(text: 'Releasing on\t-'+launch_on,size: 14,),)
-         , Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Flexible(
-                flex: 2,
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  height: 200,
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                height: 200,
 
-                  child: Image.network( 'http://image.tmdb.org/t/p/w500' +posterurl,fit: BoxFit.cover,alignment: Alignment.center,),
-                ),
+                child: Image.network( 'http://image.tmdb.org/t/p/w500' +posterurl,fit: BoxFit.cover,alignment: Alignment.center,),
               ),
-              Flexible(flex: 2,
-                child: Container(
-                  child: modified_text(text: description,size: 18,),
-                ),
-              )
-            ],
-          )
+            ),
+            Flexible(flex: 2,
+              child: Container(
+                child: modified_text(text: description,size: 18,),
+              ),
+            )
           ],
-        ),
+        )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie/constant.dart';
 import 'package:movie/utils/text.dart';
+import 'package:movie/widgets/customGrid.dart';
 import 'package:movie/widgets/searchBar.dart';
-import 'package:movie/widgets/toprated.dart';
-import 'package:movie/widgets/trending.dart';
 import 'package:movie/widgets/tv.dart';
 import 'package:tmdb_api/tmdb_api.dart';
 
@@ -35,15 +34,17 @@ class _HomeState extends State<Home> {
     loadMovies();
     super.initState();
   }
+
   late TMDB tmdbWithCustomLogs;
 
   List trendingmovies = [];
-  List topratedmovies=[];
+  List topratedmovies = [];
   List tv = [];
 
+  List<bool> _selections = List.generate(3, (i) => false);
 
   loadMovies() async {
-     tmdbWithCustomLogs = TMDB(ApiKeys(apiKey, apiReadAccessToken),
+    tmdbWithCustomLogs = TMDB(ApiKeys(apiKey, apiReadAccessToken),
         logConfig: ConfigLogger(
           showLogs: true,
           showErrorLogs: true,
@@ -60,30 +61,104 @@ class _HomeState extends State<Home> {
     });
     print(trendingmovies);
   }
-onItemChanged(v)async{
-  Map search=await tmdbWithCustomLogs.v3.search.queryMovies(v.toString().toLowerCase());
-  setState(() {
-    newList=search['results'];
-  });
-  print("_____________________________________${newList}");
-}
+
+  onItemChanged(v) async {
+    Map search = await tmdbWithCustomLogs.v3.search
+        .queryMovies(v.toString().toLowerCase());
+    setState(() {
+      newList = search['results'];
+    });
+    print("_____________________________________${newList}");
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(    backgroundColor: kbackgroundColor,
+    return Scaffold(
+      backgroundColor: kbackgroundColor,
       appBar: AppBar(
         backgroundColor: kbackgroundColor,
-        title: modified_text(text: 'Flutter Movie App❤️',),
-
+        title: modified_text(
+          text: 'Flutter Movie App❤️',
+        ),
       ),
       body: ListView(
-      children: [
-        SearchMovies(onItemChanged:onItemChanged),
-        TrendingMovies(trending:  newList.isEmpty? trendingmovies:newList,),
-        TV(tv:newList.isEmpty?  tv:newList,),
-        TopRated(toprated: newList.isEmpty? topratedmovies:newList,),
+        children: [
+          SearchMovies(onItemChanged: onItemChanged),
+          addVerticalSize(),
+          ToggleButtons(
+            isSelected: _selections,
+            onPressed: (int index) {
+              setState(() {
+                _selections[index] = !_selections[index];
 
-      ],
+                if (index == 0 && _selections[index]) {
+                  newList = trendingmovies;
+                } else if (index == 1 && !_selections[index]) {
+                  newList = topratedmovies;
+                } else if (index == 2 && !_selections[index]) {
+                  newList = [];
+                }
+              });
+            },
+            children: [
+              toggleButton(buttonName: "Most Popular"),
+              toggleButton(buttonName: "Top Rated"),
+              toggleButton(buttonName: "Reset"),
+            ],
+          ),
+          showList(
+              list: newList.isEmpty ? trendingmovies : newList,
+              listname: newList == trendingmovies
+                  ? "Trending Movies"
+                  : newList.isEmpty
+                      ? "Trending Movies"
+                      : "Top Rated Movies"),
+          showList(
+              list: newList.isEmpty ? topratedmovies : newList,
+              listname: newList == topratedmovies
+                  ? "Top Rated Movies"
+                  : newList.isEmpty
+                      ? "Trending Movies"
+                      : "Trending Movies"),
+          TV(
+            tv: newList.isEmpty ? tv : newList,
+          ),
+        ],
+      ),
+    );
+  }
+
+  toggleButton({required String buttonName}) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      margin: const EdgeInsets.only(left: 8),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(33),
+          border: Border.all(color: Colors.white)),
+      child: modified_text(
+        text: buttonName,
+      ),
+    );
+  }
+
+  showList({required List list, required String listname}) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          modified_text(
+            text: listname,
+            size: 26,
+          ),
+          addVerticalSize(),
+          GridCustom(
+            list: list,
+            path: 'poster_path',
+            namePath: 'title',
+            mainHeight: 900.0,
+          ),
+        ],
       ),
     );
   }
